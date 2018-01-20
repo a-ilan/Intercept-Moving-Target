46b8574909bdeec7e0fa414f9d35485849031c1b
Amitai Ilan <amitai.ilan@gmail.com>
2018-01-20 07:53:01 -0500

    refactoring

--- README.md
+++ README.md
@@ -4,3 +4,19 @@ npm install        # install dependencies
 npm run build      # build production release in /dist directory
 npm start          # start dev server at localhost:8080
 ```
+
+### Finding collision
+```
+collisionPoint = target + targetVelocity*time;
+distance = length(collisionPoint - source);
+distance = length(target + targetVelocity*time - source);
+time = distance/sourceSpeed;
+time = length(target + targetVelocity*time - source)/sourceSpeed;
+0 = length(target+targetVelocity*time-source) - time*sourceSpeed;	
+0 = (target.x-source.x + t*targetVelocity.x)^2 + (target.y-source.y + t*targetVelocity.y)^2 + (target.z-source.z + t*targetVelocity.z)^2 - t^2 * sourceSpeed^2
+
+var a = targetVelocity.x*targetVelocity.x + targetVelocity.y*targetVelocity.y + targetVelocity.z*targetVelocity.z - sourceSpeed*sourceSpeed;
+var b = 2*( targetVelocity.x*(target.x - source.x) + targetVelocity.y*(target.y - source.y) + targetVelocity.z*(target.z - source.z) ); 
+var c = (target.x - source.x)*(target.x - source.x) + (target.y - source.y)*(target.y - source.y) + (target.z - source.z)*(target.z - source.z); 
+var time = solve_quadratic_equation(a,b,c);
+```
--- src/index.js
+++ src/index.js
@@ -8,7 +8,8 @@ app.controller('myCtrl', [ '$scope', function($scope) {
 	$scope.targetDestination = "(500,100,300)";
 	$scope.sourceSpeed = 1;
 	$scope.targetSpeed = 5;
-	$scope.result = {err: null, time: 0, intersection: '', intersection2: ''};
+	$scope.result = {time: 0, collisionPosition: '', sourceDestination: ''};
+	$scope.err = null;
 	
 	$scope.calc = function(){
 		try {
@@ -18,8 +19,9 @@ app.controller('myCtrl', [ '$scope', function($scope) {
 			var sourceSpeed = $scope.sourceSpeed;
 			var targetSpeed = $scope.targetSpeed;
 			$scope.result = get_result(source, sourceSpeed, target, targetSpeed, targetDestination);
+			$scope.err = null;
 		} catch (err){
-			$scope.result.err = err;
+			$scope.err = err;
 		}
 	};
 	$scope.calc();
@@ -41,18 +43,21 @@ function round(value,decimals=2){
 }
 
 function get_result(source, sourceSpeed, target, targetSpeed, targetDestination){
+	if(sourceSpeed < 0 || targetSpeed < 0) throw "Invalid speed";
 	var targetDirection = get_direction(target,targetDestination);
 	var targetVelocity = get_velocity(targetDirection,targetSpeed);
-	var time = get_collision_time(source, sourceSpeed, target, targetVelocity);
-	var intersection = get_intersection(target,targetVelocity,time);
-	var sourceDirection = get_direction(source,intersection);
+	var collisionTime = get_collision_time(source, sourceSpeed, target, targetVelocity);
+	var targetArrivalTime = get_time(target,targetDestination,targetSpeed);
+	if(collisionTime >= targetArrivalTime)
+		throw "Target reaches its destination before the collision. Target arrival time: " + targetArrivalTime + ", Collision time: " + collisionTime + ".";
+	var collisionPosition = get_destination(target,targetVelocity,collisionTime);
+	var sourceDirection = get_direction(source,collisionPosition);
 	var sourceVelocity = get_velocity(sourceDirection,sourceSpeed);
-	var intersection2 = get_intersection(source,sourceVelocity,time);
+	var sourceDestination = get_destination(source,sourceVelocity,collisionTime);
 	return {
-		err: null,
-		time: time,
-		intersection: stringifyVector(intersection), 
-		intersection2: stringifyVector(intersection2),
+		time: collisionTime,
+		collisionPosition: stringifyVector(collisionPosition), 
+		sourceDestination: stringifyVector(sourceDestination),
 	};
 }
 
@@ -60,30 +65,13 @@ function get_collision_time(source, sourceSpeed, target, targetVelocity){
 	var a = targetVelocity.x*targetVelocity.x + targetVelocity.y*targetVelocity.y + targetVelocity.z*targetVelocity.z - sourceSpeed*sourceSpeed;
 	var b = 2*( targetVelocity.x*(target.x - source.x) + targetVelocity.y*(target.y - source.y) + targetVelocity.z*(target.z - source.z) ); 
 	var c = (target.x - source.x)*(target.x - source.x) + (target.y - source.y)*(target.y - source.y) + (target.z - source.z)*(target.z - source.z); 
-	if(a == 0) return -c/b;
+	if(a == 0) return -c/b; // t = -c/b
 	var result = solve_quadratic_equation(a,b,c);
 	if(result[0] < 0 && result[1] < 0) throw "Invalid collision time.";
 	if(result[0] < 0) return result[1];
 	if(result[1] < 0) return result[0];
 	if(result[0] < result[1]) return result[0];
 	return result[1];
-}
-
-function get_intersection(position,velocity,time){
-	return {x: position.x + time*velocity.x, 
-		y: position.y + time*velocity.y,
-		z: position.z + time*velocity.z };
-}
-
-function get_direction(source,destination){
-	var diff = {x: destination.x - source.x, y: destination.y - source.y, z: destination.z - source.z};
-	var mag = Math.sqrt((diff.x*diff.x)+(diff.y*diff.y)+(diff.z*diff.z));
-	diff.x /= mag; diff.y /= mag; diff.z /= mag;
-	return diff;
-}
-
-function get_velocity(direction, speed){
-	return {x: direction.x*speed, y: direction.y*speed, z: direction.z*speed};
 }
 
 function solve_quadratic_equation(a,b,c){
@@ -95,3 +83,31 @@ function solve_quadratic_equation(a,b,c){
 	var x2 = (-b - Math.sqrt(must_be_positive))/(2*a);
 	return [x1, x2];
 } 
+
+function get_destination(source,velocity,time){
+	return {x: source.x + time*velocity.x, 
+		y: source.y + time*velocity.y,
+		z: source.z + time*velocity.z };
+}
+
+function get_time(source,destination,speed){
+	var diff = {x: destination.x - source.x, y: destination.y - source.y, z: destination.z - source.z};
+	var length = get_length(diff);
+	return length/speed;
+}
+
+function get_direction(source,destination){
+	var diff = {x: destination.x - source.x, y: destination.y - source.y, z: destination.z - source.z};
+	var length = get_length(diff);
+	diff.x /= length; diff.y /= length; diff.z /= length;
+	return diff;
+}
+
+function get_velocity(direction, speed){
+	return {x: direction.x*speed, y: direction.y*speed, z: direction.z*speed};
+}
+
+function get_length(vec){
+	//magnitude/norm/length of a vector
+	return Math.sqrt((vec.x*vec.x)+(vec.y*vec.y)+(vec.z*vec.z));
+}
